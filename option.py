0d1614f62d1c6c5bfb4e603a87bafd2ba0a406ce
Alex Mangust <sanya.stogny@gmail.com>
2023-10-27 23:45:21 +0800

    Добавил файл option.py

---
+++ option.py
@@ -0,0 +1,18 @@
+def print_phone_book():
+    print()
+
+def find_phone_family():
+    print()
+
+def change_phone_number():
+    print()
+
+def delete_record():
+    print()
+
+def find_subscriber_by_phone():
+    print()
+
+
+def add_subscriber_in_phonebook():
+    print()
