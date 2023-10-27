c12800e481d73a95425d8e7a642c8dea670e4a1c
Alex Mangust <sanya.stogny@gmail.com>
2023-10-28 05:32:48 +0800

    Дополнил код

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import option
-
+import sys
 
 def show_menu():
     print('1. Распечатать справочник',
@@ -9,7 +9,7 @@ def show_menu():
           '5. Найти абонента по номеру телефона',
           '6. Добавить абонента в справочник',
           '7. Закончить работу', sep='\n')
-    choice = int(input("Выберите опцию: "))
+    choice = input("Выберите опцию: ")
     return choice
 
 
@@ -27,34 +27,35 @@ def read_txt(filename):
 def work_with_phonebooke():
     choice = show_menu()
     while choice != 7:
-        if choice == 1:
-            print("Опция 1 выбрана: Распечатать справочник", end="\n\n")
-            option.print_phone_book(read_txt("phonebook.txt"))
-        elif choice == 2:
-            print("Опция 2 выбрана: Найти телефон по фамилии", end="\n\n")
-            option.find_phone_family(read_txt("phonebook.txt"))
-        elif choice == 3:
-            print("Опция 3 выбрана: Изменить номер телефона")
-            option.change_phone_number(
-                read_txt("phonebook.txt"), "phonebook.txt")
-        elif choice == 4:
-            print("Опция 4 выбрана: Удалить запись")
-            option.delete_record(read_txt("phonebook.txt"), "phonebook.txt")
-        elif choice == 5:
-            print("Опция 5 выбрана: Найти абонента по номеру телефона")
-            option.find_subscriber_by_phone(read_txt("phonebook.txt"))
-        elif choice == 6:
-            print("Опция 6 выбрана: Добавить абонента в справочник")
-            option.add_subscriber_in_phonebook(
-                read_txt("phonebook.txt"), "phonebook.txt")
+        if choice == "end" or choice == "End":
+            sys.exit()
+        elif choice.isdigit() == False:
+            print("Недопустимый выбор. Выберите от 1 до 7.\n\n")
         else:
-            print("Недопустимый выбор. Выберите от 1 до 7.")
-        print(end="\n\n")
-        next = int(input("Чтобы продолжить, введите 1: "))
-        if next == 1:
-            choice = show_menu()
-        else:
-            choice = 7
+            choice = int(choice)
+            if choice == 1:
+                print("Опция 1 выбрана: Распечатать справочник", end="\n\n")
+                option.print_phone_book(read_txt("phonebook.csv"))
+            elif choice == 2:
+                print("Опция 2 выбрана: Найти телефон по фамилии", end="\n\n")
+                option.find_phone_family(read_txt("phonebook.csv"))
+            elif choice == 3:
+                print("Опция 3 выбрана: Изменить номер телефона")
+                option.change_phone_number(read_txt("phonebook.csv"), "phonebook.csv")
+            elif choice == 4:
+                print("Опция 4 выбрана: Удалить запись")
+                option.delete_record(read_txt("phonebook.csv"), "phonebook.csv")
+            elif choice == 5:
+                print("Опция 5 выбрана: Найти абонента по номеру телефона")
+                option.find_subscriber_by_phone(read_txt("phonebook.csv"))
+            elif choice == 6:
+                print("Опция 6 выбрана: Добавить абонента в справочник")
+                option.add_subscriber_in_phonebook(read_txt("phonebook.csv"), "phonebook.csv")
+            else:
+                print("Недопустимый выбор. Выберите от 1 до 7.")
+            print(end="\n\n")
+            next = input("Чтобы продолжить, введите любое значение: ")
+        choice = show_menu()
 
 
 work_with_phonebooke()
