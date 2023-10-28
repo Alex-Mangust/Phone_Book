89b6ceee74bf27b93987945a99d2b5e211b9518f
Alex Mangust <sanya.stogny@gmail.com>
2023-10-28 15:31:28 +0800

    Сделал небольшие улучшения

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import sys
 def show_menu():
     print('1. Распечатать справочник',
           '2. Найти телефон по фамилии',
-          '3. Изменить номер телефона',
-          '4. Удалить запись',
-          '5. Найти абонента по номеру телефона',
-          '6. Добавить абонента в справочник',
+          '3. Найти абонента по номеру телефона',
+          '4. Добавить абонента в справочник',
+          '5. Изменить номер телефона',
+          '6. Удалить запись',
           '7. Закончить работу', sep='\n')
     choice = input("Выберите опцию: ")
     return choice
@@ -27,9 +27,7 @@ def read_txt(filename):
 def work_with_phonebooke():
     choice = show_menu()
     while choice != 7:
-        if choice == "end" or choice == "End":
-            sys.exit()
-        elif choice.isdigit() == False:
+        if choice.isdigit() == False:
             print("Недопустимый выбор. Выберите от 1 до 7.\n\n")
         else:
             choice = int(choice)
@@ -40,17 +38,19 @@ def work_with_phonebooke():
                 print("Опция 2 выбрана: Найти телефон по фамилии", end="\n\n")
                 option.find_phone_family(read_txt("phonebook.csv"))
             elif choice == 3:
-                print("Опция 3 выбрана: Изменить номер телефона")
-                option.change_phone_number(read_txt("phonebook.csv"), "phonebook.csv")
-            elif choice == 4:
-                print("Опция 4 выбрана: Удалить запись")
-                option.delete_record(read_txt("phonebook.csv"), "phonebook.csv")
-            elif choice == 5:
-                print("Опция 5 выбрана: Найти абонента по номеру телефона")
+                print("Опция 3 выбрана: Найти абонента по номеру телефона")
                 option.find_subscriber_by_phone(read_txt("phonebook.csv"))
-            elif choice == 6:
-                print("Опция 6 выбрана: Добавить абонента в справочник")
+            elif choice == 4:
+                print("Опция 4 выбрана: Добавить абонента в справочник")
                 option.add_subscriber_in_phonebook(read_txt("phonebook.csv"), "phonebook.csv")
+            elif choice == 5:
+                print("Опция 5 выбрана: Изменить номер телефона")
+                option.change_phone_number(read_txt("phonebook.csv"), "phonebook.csv")
+            elif choice == 6:
+                print("Опция 6 выбрана: Удалить запись")
+                option.delete_record(read_txt("phonebook.csv"), "phonebook.csv")
+            elif choice == 7:
+                sys.exit()
             else:
                 print("Недопустимый выбор. Выберите от 1 до 7.")
             print(end="\n\n")
