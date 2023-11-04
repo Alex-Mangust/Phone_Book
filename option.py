3101e73c9e8895dbde86d8c5bb6af62eed1ee55c
Alex Mangust <sanya.stogny@gmail.com>
2023-11-05 03:34:30 +0800

    Исправил некоторые ошибки

--- option.py
+++ option.py
@@ -30,43 +30,88 @@ def find_phone_family(file_open):
         print("Абонент не найден")
 
 
-# 3. Изменить какое-либо  значение
-def change(file_open, filename):
-    fields = ['Фамилия', 'Имя', 'Телефон', 'Описание']
-    print("По какому параметру хотите производить поиск?\n1. Фамилия\n2. Имя \n3. Телефон\n4. Описание")
-    field_input = int(input())
-    field_in = fields[field_input - 1]
-    famile_found = input("Введите свой запрос: ")
-    found_people = []
-    id = []
-    for record in range(len(file_open)):
-        if file_open[record].get(field_in) == famile_found:
-            found_people.append(file_open[record])
-            id.append(record)
-    if len(found_people) >= 2:
-        print("Найдено больше одного запроса, выберите 1")
-        for i in range(len(found_people)):
-            line = ', '.join('{} {}'.format(key, val) for key, val in sorted(found_people[i].items()))
-            # print(*i.values())
-            print(f"{i+1} - {line}")
-        vi = int(input())
-        vi = vi - 1
-    else:
-        vi = 0
-    print("Что вы хотите изменить?\n1. Фамилия\n2. Имя \n3. Телефон\n4. Описание")
-    change = int(input())
-    change_in = fields[change - 1]
-    new_record = input("Введите нове значение: ")
+# 3. Найти абонента по номеру телефона
+def find_subscriber_by_phone(file_open):
+    phone = input("Введите номер телефона абонента: ")
+    found_phone_record = []
+    found_fio_record = []
+    found_info_record = []
     for record in file_open:
-        if record == found_people[vi]:
-            record[change_in] = new_record
+        if record.get("Телефон") == phone:
+            found_phone_record.append(record.get("Телефон"))
+            found_fio_record.append(record.get("Фамилия"))
+            found_fio_record.append(record.get("Имя"))
+            found_info_record.append(record.get("Описание"))
+    fio = " ".join(found_fio_record)
+    info = "".join(found_info_record)
+    if found_phone_record:
+        for record in found_phone_record:
+            print(f"Абонент {fio}. Телефон - {record}. Описание - {info}")
+    else:
+        print("Абонент не найден")
+
+
+# 4. Добавить абонента в справочник
+def add_subscriber_in_phonebook(file_open, filename):
+    fields = ['Фамилия', 'Имя', 'Телефон', 'Описание']
+    family = input("Введите фамилию абонента: ")
+    name = input("Введите имя абонента: ")
+    phone = input("Введите номер телефона абонента: ")
+    info = input("Введите описание: ")
+    new_subscriber = [family, name, phone, info]
+    new_subscriber = dict(zip(fields, new_subscriber))
+    file_open.append(new_subscriber)
+    print(file_open)
     with open(filename, 'w', encoding='utf-8') as phb:
         for record in file_open:
             line = ','.join(record[field] for field in fields)
             phb.write(line + "\n")
 
+# 5. Изменить контакт
+def change(file_open, filename):
+    next_change = True
+    while (next_change):
+        fields = ['Фамилия', 'Имя', 'Телефон', 'Описание']
+        print("По какому параметру хотите производить поиск?\n1. Фамилия\n2. Имя \n3. Телефон\n4. Описание")
+        field_input = int(input())
+        field_in = fields[field_input - 1]
+        famile_found = input("Введите свой запрос: ")
+        found_people = []
+        id = []
+        for record in range(len(file_open)):
+            if file_open[record].get(field_in) == famile_found:
+                found_people.append(file_open[record])
+                id.append(record)
+        if len(found_people) >= 2:
+            print("Найдено больше одного запроса, выберите 1")
+            for i in range(len(found_people)):
+                line = ', '.join('{} {}'.format(key, val) for key, val in sorted(found_people[i].items()))
+                # print(*i.values())
+                print(f"{i+1} - {line}")
+            vi = int(input())
+            vi = vi - 1
+        elif len(found_people) == 0:
+            print("Запись не найдена!")
+            return 0
+        else:
+            vi = 0
+        print("Что вы хотите изменить?\n1. Фамилия\n2. Имя \n3. Телефон\n4. Описание")
+        change = int(input())
+        change_in = fields[change - 1]
+        new_record = input("Введите нове значение: ")
+        for record in file_open:
+            if record == found_people[vi]:
+                record[change_in] = new_record
+        with open(filename, 'w', encoding='utf-8') as phb:
+            for record in file_open:
+                line = ','.join(record[field] for field in fields)
+                phb.write(line + "\n")
+        next_input = int(input("Хотите поменять что-либо еще?\n1. Да   2. Нет\n"))
+        if next_input == 2:
+            next_change = False
 
-# 4. Удалить запись
+
+# 6. Удалить запись
 def delete_record(file_open, filename):
     fields = ['Фамилия', 'Имя', 'Телефон', 'Описание']
     confirm_w = False
@@ -104,41 +149,6 @@ def delete_record(file_open, filename):
                     confirm_w = False
         else:
             print("Абонент не найден")
-
-
-# 5. Найти абонента по номеру телефона
-def find_subscriber_by_phone(file_open):
-    phone = input("Введите номер телефона абонента: ")
-    found_phone_record = []
-    found_fio_record = []
-    found_info_record = []
-    for record in file_open:
-        if record.get("Телефон") == phone:
-            found_phone_record.append(record.get("Телефон"))
-            found_fio_record.append(record.get("Фамилия"))
-            found_fio_record.append(record.get("Имя"))
-            found_info_record.append(record.get("Описание"))
-    fio = " ".join(found_fio_record)
-    info = "".join(found_info_record)
-    if found_phone_record:
-        for record in found_phone_record:
-            print(f"Абонент {fio}. Телефон - {record}. Описание - {info}")
-    else:
-        print("Абонент не найден")
-
-
-# 6. Добавить абонента в справочник
-def add_subscriber_in_phonebook(file_open, filename):
-    fields = ['Фамилия', 'Имя', 'Телефон', 'Описание']
-    family = input("Введите фамилию абонента: ")
-    name = input("Введите имя абонента: ")
-    phone = input("Введите номер телефона абонента: ")
-    info = input("Введите описание: ")
-    new_subscriber = [family, name, phone, info]
-    new_subscriber = dict(zip(fields, new_subscriber))
-    file_open.append(new_subscriber)
-    print(file_open)
-    with open(filename, 'w', encoding='utf-8') as phb:
-        for record in file_open:
-            line = ','.join(record[field] for field in fields)
-            phb.write(line + "\n")
+            next = int(input("Продолжить?\n1. Да    2. Нет\n"))
+            if next == 2:
+                return 0
